7a2904df599b60879a2d06424941f7951bdec2c2
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-09-13 12:35:21 +0800

    feat: add auth view

---
+++ tujuhin/View/AuthView.swift
@@ -0,0 +1,111 @@
+//
+//  LoginView.swift
+//  tujuhin
+//
+//  Created by Tude Maha on 13/09/2025.
+//
+
+import SwiftUI
+
+enum AuthInput {
+    case name, username, password
+}
+
+struct AuthView: View {
+    @State private var loginState: Bool = true
+    
+    @State private var name: String = ""
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @FocusState private var currentFocus: AuthInput?
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Spacer()
+            
+            Text("\(loginState ? "Welcome" : "Register") to Tujuhin!")
+                .font(.title.bold())
+            
+            Spacer()
+            
+            VStack(spacing: 25) {
+                VStack(spacing: 15) {
+                    if !loginState {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Name")
+                            TextField("Name", text: $name)
+                                .frame(height: 40)
+                                .padding(.horizontal, 15)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(currentFocus == .name ? Color.crimsonRed.opacity(0.5) : Color.gray.opacity(0.5))
+                                }
+                                .focused($currentFocus, equals: .name)
+                        }
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Username")
+                        TextField("Username", text: $username)
+                            .frame(height: 40)
+                            .padding(.horizontal, 15)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(currentFocus == .username ? Color.crimsonRed.opacity(0.5) : Color.gray.opacity(0.5))
+                            }
+                            .focused($currentFocus, equals: .username)
+                            .textInputAutocapitalization(.never)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Password")
+                        SecureField("Password", text: $password)
+                            .frame(height: 40)
+                            .padding(.horizontal, 15)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(currentFocus == .password ? Color.crimsonRed.opacity(0.5) : Color.gray.opacity(0.5))
+                            }
+                            .focused($currentFocus, equals: .password)
+                    }
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text(loginState ? "Login" : "Register")
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                        .background(.crimsonRed)
+                        .clipShape(.capsule)
+                }
+                
+                HStack(spacing: 5) {
+                    Text(loginState ? "Don't have an account?" : "Already have account?")
+                    Text(loginState ? "Register" : "Login")
+                        .foregroundStyle(.crimsonRed)
+                        .onTapGesture {
+                            loginState.toggle()
+                        }
+                }
+            }
+            
+            Spacer()
+            Spacer()
+        }
+        .padding(.horizontal, 40)
+        .onChange(of: loginState) {
+            if loginState {
+                currentFocus = .username
+            } else {
+                currentFocus = .name
+            }
+        }
+    }
+}
+
+#Preview {
+    AuthView()
+}
--- tujuhin/tujuhinApp.swift
+++ tujuhin/tujuhinApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct tujuhinApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            AuthView()
         }
     }
 }
